d334371b9c266c066602d338d62d23457b92cbee
Matteo Tornaboni <108476185+matteotorna@users.noreply.github.com>
2024-04-05 01:08:07 +0200

    bug fix

--- example.js
+++ example.js
@@ -2,21 +2,24 @@ const r6Info = require('.');
 
 async function main() {
   try {
-    // Resetta e rigenera il token
-    await r6Info.tokenManager.generateToken();  // Aggiungi await qui
-    console.log("Token:", r6Info.tokenManager.getToken());
+    //Resetta e rigenera il token
+    //await r6Info.tokenManager.generateToken(); 
+    const { token, callId } = await r6Info.tokenManager.getToken();
+    console.log(token, callId);
+  
+    //console.log("Token:", r6Info.tokenManager.getToken(token, callId));
 
-    // Verifica se il token è valido dopo la rigenerazione
-    if (r6Info.tokenManager.isValidToken()) {
+    // // Verifica se il token è valido dopo la rigenerazione
+    if (await r6Info.tokenManager.isValidToken()) {
       console.log("Il token è valido.");
-      
-      // Prova a ottenere i dati
-      const mapsData = await r6Info.getMaps({ releaseDate: "2019" });
-      console.log(mapsData);
-    } else {
-      console.log("Il token non è valido.");
-      // Qui puoi gestire il caso in cui il token non è valido.
     }
+    //   // Prova a ottenere i dati
+    //   const mapsData = await r6Info.getMaps({ releaseDate: "2019" });
+    //   console.log(mapsData);
+    // } else {
+    //   console.log("Il token non è valido.");
+    //   // Qui puoi gestire il caso in cui il token non è valido.
+    // }
   } catch (error) {
     console.error('Errore durante le richieste:', error.message);
   }
--- methods/tokenManager.js
+++ methods/tokenManager.js
@@ -1,49 +1,57 @@
 const axiosInstance = require('../axiosInstance/axiosInstance');
-const ms = require('ms');
 
 let tokenData = {
   token: null,
-  expiresIn: null,
-  callDateTime: null,
   callId: null
 };
 
 async function generateToken() {
   try {
-    const response = await axiosInstance.get('/token');
-    const { token, expiresIn, callDateTime, callId } = response.data;
-
+    const response = await axiosInstance.post('/token');
+    console.log('Token generated with response:', response.data);
     tokenData = { 
-      token, 
-      expiresIn: ms(expiresIn), 
-      callDateTime, 
-      callId 
+      token: response.data.token, 
+      callId: response.data.callId 
     };
+    console.log('Token data after generation:', tokenData);
   } catch (error) {
     console.error('Error during the token request:', error.message);
     throw error;
   }
 }
 
-function isValidToken() {
-  if (!tokenData.token || !tokenData.expiresIn || !tokenData.callDateTime) {
-    return false;
+async function isValidToken() {
+  try {
+    const response = await axiosInstance.post('/tokenManagement', { action: 'verify' });
+    return response.data.isValid;
+  } catch (error) {
+    console.error('Error during token validation:', error.message);
+    throw error;
   }
-  const formattedCallDateTime = tokenData.callDateTime.replace(/(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2}):(\d{2})/, '$3-$2-$1T$4:$5:$6Z');
-  const expirationDateTime = new Date(new Date(formattedCallDateTime).getTime() + tokenData.expiresIn);
-  
-  return new Date() < expirationDateTime;
 }
 
-function clearToken() {
-  tokenData = { token: null, expiresIn: null, callDateTime: null, callId: null };
+async function clearToken() {
+  try {
+    await axiosInstance.post('/tokenManagement', { action: 'delete' });
+    tokenData = { token: null, callId: null };
+  } catch (error) {
+    console.error('Error during token deletion:', error.message);
+    throw error;
+  }
 }
 
-function getToken() {
-  return {
-    token: tokenData.token,
-    callId: tokenData.callId,
-  };
+async function getToken() {
+  try {
+    const response = await axiosInstance.post('/tokenManagement', { action: 'get' });
+    if (response.data.token) {
+      tokenData = { token: response.data.token, callId: response.data.callId };
+      return tokenData;
+    }
+    throw new Error('Token not found');
+  } catch (error) {
+    console.error('Error during token retrieval:', error.message);
+    throw error;
+  }
 }
 
 module.exports = {
